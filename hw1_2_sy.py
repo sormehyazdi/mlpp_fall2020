b88e25c17380a0a8c5a70df6149fb5b41ee9e844
sormehyazdi <sormehyazdi@gmail.com>
2020-09-18 13:57:20 -0700

    Trying to make the variable names generalizable

--- hw1_2_sy.py
+++ hw1_2_sy.py
@@ -10,6 +10,8 @@ Purpose: Provided csv data table from hw1_sy.py file, and config_hw1.py params f
 import psycopg2 as pg2
 from config_hw1 import db_params
 
+csv_filename = 'data_census_scrape.csv'
+
 def connect_db(db_params):
   conn = pg2.connect(
     host=db_params['host'],
@@ -20,20 +22,42 @@ def connect_db(db_params):
   )
   return conn
 
+def prepare_sql_query(csv_filename):
+
+  with open(csv_filename) as f:
+    header_row = f.readlines()[0]
+
+  variables = header_row.split(",")
+  sql_execute = "CREATE TABLE IF NOT EXISTS public.sy_demography ("
+
+  for v in variables:
+    sql_execute = sql_execute + v + ' integer,'
+
+  ## Dropping the last comma so that the code runs properly
+  sql_execute = sql_execute[:-1]
+
+  ## Final String to go into the curs.execute command:
+  sql_execute_str = sql_execute + ");"
+
+  return sql_execute_str
+
 def main():
   conn = connect_db(db_params)
 
+  sql_query = prepare_sql_query(csv_filename)
+
   with conn.cursor() as curs:
-      print(conn)
-      print(curs)
-      curs.execute("CREATE TABLE IF NOT EXISTS public.sy_FLdemography (lan_B06007_002E integer, lan_B06007_003E integer, lan_B06007_004E integer, lan_B06007_005E integer, lan_B06007_006E integer, inc_B06010_002E integer, inc_B06010_003E integer, inc_B06010_004E integer, inc_B06010_005E integer, inc_B06010_006E integer, inc_B06010_007E integer, inc_B06010_008E integer, inc_B06010_009E integer, inc_B06010_010E integer, inc_B06010_011E integer, educ_B06009_002E integer, educ_B06009_003E integer, educ_B06009_004E integer, educ_B06009_005E integer, educ_B06009_006E integer, home_B06008_007E integer, state VARCHAR(20), county VARCHAR(20), tract VARCHAR(20));")
-      curs.execute("DELETE FROM sy_FLdemography")
-      print(curs)
-      f = open('data_census_scrape.csv')
+      #print(conn)
+      #print(curs)
+      curs.execute(sql_query)
+      #curs.execute("CREATE TABLE IF NOT EXISTS public.sy_demography (lan_B06007_002E integer, lan_B06007_003E integer, lan_B06007_004E integer, lan_B06007_005E integer, lan_B06007_006E integer, inc_B06010_002E integer, inc_B06010_003E integer, inc_B06010_004E integer, inc_B06010_005E integer, inc_B06010_006E integer, inc_B06010_007E integer, inc_B06010_008E integer, inc_B06010_009E integer, inc_B06010_010E integer, inc_B06010_011E integer, educ_B06009_002E integer, educ_B06009_003E integer, educ_B06009_004E integer, educ_B06009_005E integer, educ_B06009_006E integer, home_B06008_007E integer, state VARCHAR(20), county VARCHAR(20), tract VARCHAR(20));")
+      curs.execute("DELETE FROM sy_demography")
+      #print(curs)
+      f = open(csv_filename)
      # curs.copy_from(f, 'sytesttable',columns=('lan1', 'lan2', 'lan3', 'lan4', 'lan5', 'inc1', 'inc2', 'inc3', 'inc4', 'inc5', 'inc6', 'inc7', 'inc8', 'inc9', 'inc10','educ1', 'educ2', 'educ3', 'educ4', 'educ5', 'home1', 'state', 'county', 'tract'), sep=",")
-      copy_stat = "COPY sy_FLdemography FROM stdin DELIMITER \',\' CSV header;"
+      copy_stat = "COPY sy_demography FROM stdin DELIMITER \',\' CSV header;"
       curs.copy_expert(copy_stat, f)
-      print(curs)
+      #print(curs)
 
   conn.commit()
   conn.close()
